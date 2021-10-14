2672c47f3ccdcd41a02488dbe640c8084897ddfd
Spwstop <suppawat.sa@ku.th>
2021-10-14 15:10:56 +0700

    add update method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -98,4 +98,17 @@ class AuthController extends Controller
             'user' => auth()->user()
         ]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->name = $request->input('name');
+        $user->age = $request->input('age');
+        $user->gender = $request->input('gender');
+        $user->save();
+
+
+        return $user ;
+    }
 }
